f995cf91f2359ab0af0a15a1ee45aab3e06464bc
curiosity110 <91900298+curiosity110@users.noreply.github.com>
2025-08-18 10:25:39 +0200

    feat: add full product listing page

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -8,3 +8,8 @@ class IndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "index.html")
 
+    def test_products_page_returns_success(self):
+        response = self.client.get(reverse("products"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "all_products.html")
+
--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def products_page(request):
+    return render(request, 'all_products.html')
